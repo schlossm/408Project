4bf85c8bef9ea3e1dec9aa4d3328ca453bf85f78
rosenbea <rosenbea@purdue.edu>
2016-09-18 18:45:10 -0400

    Basic local public API working (no db)
    User constructor
    getUsername
    makeUser
    makeMod
    makeAdmin
    getUserType
    ban
    unban
    isBanned
    toString

--- src/objects/User.java
+++ src/objects/User.java
@@ -9,12 +9,78 @@ import JSON_translation.*;
  */
 public class User {
 	private String username;
+	public enum UserType {
+		USER, MOD, ADMIN
+	}
+	private UserType userType;
+	private boolean isBanned;
 	
 	public User(String username) {
+		/*
+		 * TODO: Get values from database
+		 */
 		this.username = username;
+		this.userType = UserType.USER;
+		this.isBanned = false;
+		
+		/*
+		 * TODO: handle new user
+		 */
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	/*
+	 * Demote to normal user status
+	 */
+	public void makeUser() {
+		/*
+		 * TODO: Set value in database
+		 */
+		this.userType = UserType.USER;
+	}
+	
+	public void makeMod() {
+		/*
+		 * TODO: Set value in database
+		 */
+		this.userType = UserType.MOD;
+	}
+	
+	public void makeAdmin() {
+		/*
+		 * TODO: Set value in database
+		 */
+		this.userType = UserType.ADMIN;
+	}
+	
+	public UserType getUserType() {
+		return this.userType;
+	}
+	
+	public void ban() {
+		/*
+		 * TODO: set value in database
+		 */
+		this.isBanned = true;
+	}
+	
+	public void unban() {
+		/*
+		 * TODO: set value in database
+		 */
+		this.isBanned = false;
+	}
+	
+	public boolean isBanned() {
+		return this.isBanned;
 	}
 	
 	public String toString() {
-		return this.username;
+		return "username:\t" + this.username + "\n"
+				+ "userType:\t" + this.userType + "\n"
+				+ "isBanned\t" + this.isBanned;
 	}
 }
