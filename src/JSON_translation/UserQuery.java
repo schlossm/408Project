12247c801bb641f985c0d676d4577330c8592821
Naveen Ganessin <naveenganessin@gmail.com>
2016-09-21 19:38:45 -0400

    Attempting database connection

--- src/JSON_translation/UserQuery.java
+++ src/JSON_translation/UserQuery.java
@@ -1,13 +1,19 @@
 package JSON_translation;
 
 import org.json.*;
+
+import com.google.gson.JsonObject;
+
 import database.DFDatabase;
+import database.DFDatabaseCallbackDelegate;
+import database.DFError;
 import database.dfDatabaseFramework.DFSQL.DFSQL;
 import database.dfDatabaseFramework.DFSQL.DFSQLError;
+import database.dfDatabaseFramework.WebServerCommunicator.DFDataUploaderReturnStatus;
 import objects.User;
 import objects.User.UserType;
 
-public class UserQuery {
+public class UserQuery implements DFDatabaseCallbackDelegate{
 	public User getUser(String username){
 		String userJSONStr, usernameRecieved;
 		UserType userType;
@@ -23,6 +29,10 @@ public class UserQuery {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		
+		//get UsERpRIVILIGE
+		//GET 
+		//DFDATABASE.defaultdatabase.executedsplstaement()
 		
 		userJSONStr = "";
 		JSONObject userJSON;
@@ -44,14 +54,34 @@ public class UserQuery {
 		return requestedUser;
 	}
 	
+	public boolean getUserBanStatus(String username){
+		String userJSONStr, usernameRecieved;
+		boolean isBanned;
+		DFSQL dfsql = new DFSQL();
+		try {
+			dfsql.select("banned").from("User").whereEquals("userID", username);
+			DFDatabase.defaultDatabase.executeSQLStatement(dfsql, this);
+		} catch (DFSQLError e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		return true;
+	}
+		
+	//Getter 
+	
 	public boolean addNewUser(String userName, String pw, UserType userType){
 		//INSERT INTO USER VALUES (userName, pw, userTypeInt, isbanned)
 		int convertedUserType = userPriviligeEnumToIntConverter(userType);
-		String[] rows = {"UserID", "password", "priviligeLevel", "banned"};
-		String[] values = {userName, pw,  String.valueOf(convertedUserType), String.valueOf(false)};
+
+		String[] rows = {"userID", "password", "privilegeLevel", "banned"};
+		String[] values = {userName, pw,  String.valueOf(convertedUserType), String.valueOf(0)};
 		DFSQL dfsql = new DFSQL();
 		try {
 			dfsql.insert("User", values, rows);
+			System.out.println(dfsql.formattedSQLStatement);
+			DFDatabase.defaultDatabase.executeSQLStatement(dfsql, this);
 		} catch (DFSQLError e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -96,21 +126,36 @@ public class UserQuery {
 	public boolean verifyUserLogin(String UserName, String password){
 		return false;
 	}
+
+	@Override
+	public void returnedData(JsonObject jsonObject, DFError error) {
+		System.out.println(error.code);
+		System.out.println(error.description);
+		System.out.println(error.userInfo);
+	}
+
+	@Override
+	public void uploadStatus(DFDataUploaderReturnStatus success, DFError error) {
+		if(success == DFDataUploaderReturnStatus.success){
+			System.out.println("success uploading this");
+		} else if (success == DFDataUploaderReturnStatus.failure) {
+			System.out.println("Faiylure uploading this");
+		}
+		else if(success == DFDataUploaderReturnStatus.error){
+			System.out.println("Error uploading this");
+			System.out.println(error.code);
+			System.out.println(error.description);
+			System.out.println(error.userInfo);
+		} else {
+			System.out.println("I have no clue!");
+		}
+	}
 	
 	public static void main(String[] args)
 	{
-		System.out.println("This should be starting up here");
-		DFSQL dfsql = new DFSQL();
-		String[] selectedRows = {"UserID", "priviligeLevel", "banned"};
-		try {
-			//dfsql.select(selectedRows).from("User").whereEquals("userId", "whatever");
-			dfsql.update("User", "userId", "naveen").whereEquals("userid", "naveendup");
-			
-			System.out.println("Succeeds try");
-		} catch (DFSQLError e1) {
-			// TODO Auto-generated catch block
-			System.out.println("Fails try");
-			e1.printStackTrace();
-		}
+		UserQuery userQuery = new UserQuery();
+		//userQuery.getUserBanStatus("testUser");
+		userQuery.addNewUser("naveenTest1", "dasdsada", UserType.USER);
+		System.out.println("end reached");
 	}
 }
