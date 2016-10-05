9092eba3573ee7d74541dfa1a6adecb198b07909
rosenbea <rosenbea@purdue.edu>
2016-10-05 15:01:14 -0400

    Imports added to new query files

--- src/JSON_translation/PostQuery.java
+++ src/JSON_translation/PostQuery.java
@@ -1,5 +1,25 @@
 package JSON_translation;
 
+import javax.print.attribute.standard.RequestingUserName;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import UI.UIStrings;
+import UIKit.DFNotificationCenter;
+import database.DFDatabase;
+import database.DFDatabaseCallbackDelegate;
+import database.DFError;
+import database.dfDatabaseFramework.DFSQL.DFSQL;
+import database.dfDatabaseFramework.DFSQL.DFSQLError;
+import database.dfDatabaseFramework.WebServerCommunicator.DFDataUploaderReturnStatus;
+import objects.User;
+import objects.User.UserType;
+
+import objects.Debate.*;
+import objects.Post.*;
+
 public class PostQuery {
 
 }
