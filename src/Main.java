db5daaa3a64283358268c1295d756297b7387545
rramstad <rramstad@purdue.edu>
2016-10-26 14:40:43 -0400

    Style Changes

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		new Frame("School of Thought");
+		
 		while(true)
 		{
 			try
@@ -25,5 +26,6 @@ public class Main
 				System.exit(-1);
 			}
 		}
+		
 	}
 }
